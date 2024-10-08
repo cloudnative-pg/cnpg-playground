db82e264c8fb5c30ace6cc6c39e0f5ad1d18d357
Tristan Otterpohl <82106086+Otterpohl@users.noreply.github.com>
2024-10-08 07:58:44 +0100

    feat: support using podman in addition to docker (#3)
    Signed-off-by: Tristan Otterpohl <82106086+Otterpohl@users.noreply.github.com>

--- scripts/teardown.sh
+++ scripts/teardown.sh
@@ -17,11 +17,24 @@
 
 set -eu
 
+# Look for a supported container provider and use it throughout
+containerproviders="docker podman"
+for containerProvider in `which $containerproviders`; do
+    CONTAINER_PROVIDER=$containerProvider
+    break
+done
+
+# Ensure we found a supported container provider
+if [ -z ${CONTAINER_PROVIDER+x} ]; then
+    echo "Missing container provider, supported providers are $containerproviders"
+    exit 1
+fi
+
 git_repo_root=$(git rev-parse --show-toplevel)
 cd "${git_repo_root}"
 
-docker rm minio-eu -f ||:
-docker rm minio-us -f ||:
+$CONTAINER_PROVIDER rm minio-eu -f ||:
+$CONTAINER_PROVIDER rm minio-us -f ||:
 kind delete cluster --name k8s-eu ||:
 kind delete cluster --name k8s-us ||:
 rm -fr minio-eu/* minio-eu/.minio.sys ||:
