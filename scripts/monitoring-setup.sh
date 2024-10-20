ebc406f90ee70f31eb6a69cbb021b7ff087ef968
Gabriele Bartolini <gabriele.bartolini@enterprisedb.com>
2024-10-20 19:35:35 +0200

    fix: typos
    Signed-off-by: Gabriele Bartolini <gabriele.bartolini@enterprisedb.com>

--- scripts/monitoring-setup.sh
+++ scripts/monitoring-setup.sh
@@ -1,5 +1,7 @@
 # Copyright The CloudNativePG Contributors
 #
+# Setup a Prometheus/Grafana stack on infrastructure nodes
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -19,7 +21,7 @@ git_repo_root=$(git rev-parse --show-toplevel)
 kube_config_path=${git_repo_root}/k8s/kube-config.yaml
 export KUBECONFIG=${kube_config_path}
 
-# We need to deploy the Prometheus operator in both regions
+# Deploy the Prometheus operator in both Kubernetes clusters
 for context in kind-k8s-eu kind-k8s-us; do
     kubectl --context $context create ns prometheus-operator | true
     kubectl kustomize ${git_repo_root}/k8s/monitoring/prometheus-operator | \
@@ -27,8 +29,7 @@ for context in kind-k8s-eu kind-k8s-us; do
 done
 
 
-# There's dedicated nodes for infrastructure, we make sure that every reosurce
-# is attached to those nodes
+# We make sure that monitoring workloads are deployed in the infrastructure node.
 for context in kind-k8s-eu kind-k8s-us; do
     kubectl kustomize ${git_repo_root}/k8s/monitoring/prometheus-instance | \
         kubectl --context=$context apply --force-conflicts --server-side=true -f - 
@@ -43,7 +44,7 @@ for context in kind-k8s-eu kind-k8s-us; do
     kubectl --context $context apply --force-conflicts --server-side=true -f https://github.com/grafana/grafana-operator/releases/latest/download/kustomize-cluster_scoped.yaml
 done
 
-# Creating Granfa instance and dashboards
+# Creating Grafana instance and dashboards
 for context in kind-k8s-eu kind-k8s-us; do
     kubectl kustomize ${git_repo_root}/k8s/monitoring/grafana/ | \
     kubectl --context $context apply -f -
