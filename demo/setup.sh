84fb7d295a5af8e189c0acbdad07fec30cc575c9
Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>
2025-09-24 17:20:06 +0200

    fix: naming convention of Kind cluster
    This PR makes the demo script use the same naming convention used by the setup
    and teardown common scripts.
    
    Signed-off-by: Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>

--- demo/setup.sh
+++ demo/setup.sh
@@ -25,6 +25,7 @@
 
 set -eux
 
+K8S_BASE_NAME=${K8S_NAME:-k8s}
 git_repo_root=$(git rev-parse --show-toplevel)
 kube_config_path=${git_repo_root}/k8s/kube-config.yaml
 demo_yaml_path=${git_repo_root}/demo/yaml
@@ -60,51 +61,51 @@ for region in eu us; do
      # Deploy CloudNativePG operator (trunk - main branch)
      curl -sSfL \
        https://raw.githubusercontent.com/cloudnative-pg/artifacts/main/manifests/operator-manifest.yaml | \
-       kubectl --context kind-k8s-${region} apply -f - --server-side
+       kubectl --context kind-${K8S_BASE_NAME}-${region} apply -f - --server-side
    else
      # Deploy CloudNativePG operator (latest version, through the plugin)
      kubectl cnpg install generate --control-plane | \
-       kubectl --context kind-k8s-${region} apply -f - --server-side
+       kubectl --context kind-${K8S_BASE_NAME}-${region} apply -f - --server-side
    fi
 
    # Wait for CNPG deployment to complete
-   kubectl --context kind-k8s-${region} rollout status deployment \
+   kubectl --context kind-${K8S_BASE_NAME}-${region} rollout status deployment \
       -n cnpg-system cnpg-controller-manager
 
    # Deploy cert-manager
-   kubectl apply --context kind-k8s-${region} -f \
+   kubectl apply --context kind-${K8S_BASE_NAME}-${region} -f \
       https://github.com/cert-manager/cert-manager/releases/latest/download/cert-manager.yaml
 
    # Wait for cert-manager deployment to complete
-   kubectl rollout --context kind-k8s-${region} status deployment \
+   kubectl rollout --context kind-${K8S_BASE_NAME}-${region} status deployment \
       -n cert-manager
-   cmctl check api --wait=2m --context kind-k8s-${region}
+   cmctl check api --wait=2m --context kind-${K8S_BASE_NAME}-${region}
 
    if [ $trunk -eq 1 ]
    then
      # Deploy Barman Cloud Plugin (trunk)
-     kubectl apply --context kind-k8s-${region} -f \
+     kubectl apply --context kind-${K8S_BASE_NAME}-${region} -f \
        https://raw.githubusercontent.com/cloudnative-pg/plugin-barman-cloud/refs/heads/main/manifest.yaml
    else
      # Deploy Barman Cloud Plugin (latest stable)
-     kubectl apply --context kind-k8s-${region} -f \
+     kubectl apply --context kind-${K8S_BASE_NAME}-${region} -f \
         https://github.com/cloudnative-pg/plugin-barman-cloud/releases/latest/download/manifest.yaml
    fi
 
    # Wait for Barman Cloud Plugin deployment to complete
-   kubectl rollout --context kind-k8s-${region} status deployment \
+   kubectl rollout --context kind-${K8S_BASE_NAME}-${region} status deployment \
       -n cnpg-system barman-cloud
 
    # Create Barman object stores
-   kubectl apply --context kind-k8s-${region} -f \
+   kubectl apply --context kind-${K8S_BASE_NAME}-${region} -f \
      ${demo_yaml_path}/object-stores
 
    # Create the Postgres cluster
-   kubectl apply --context kind-k8s-${region} -f \
+   kubectl apply --context kind-${K8S_BASE_NAME}-${region} -f \
      ${demo_yaml_path}/${region}/pg-${region}${legacy}.yaml
 
    # Wait for the cluster to be ready
-   kubectl wait --context kind-k8s-${region} \
+   kubectl wait --context kind-${K8S_BASE_NAME}-${region} \
      --timeout 30m \
      --for=condition=Ready cluster/pg-${region}
 
