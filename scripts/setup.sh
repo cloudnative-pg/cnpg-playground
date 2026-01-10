41f2a9f6562ed7218e237c0894b943218ede414f
Zied ABID <zied.abid@jobteaser.com>
2026-01-10 08:50:13 +0100

    fix(script): setup playground with rootless podman
    Signed-off-by: Zied ABID <zied.abid@jobteaser.com>

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -86,6 +86,9 @@ for region in "${REGIONS[@]}"; do
         "${MINIO_IMAGE}" server /data --console-address ":9001" > /dev/null
 
     echo "🏗️  Creating Kind cluster '${K8S_CLUSTER_NAME}'..."
+    if [ "$CONTAINER_PROVIDER" == "podman" ]; then
+        export KIND_EXPERIMENTAL_PROVIDER=podman
+    fi
     kind create cluster --config "${kind_config_path}" --name "${K8S_CLUSTER_NAME}"
 
     echo "🏷️  Labeling nodes in '${K8S_CLUSTER_NAME}'..."
