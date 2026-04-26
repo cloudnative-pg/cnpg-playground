631f4238aae221ab4d048e0dd9126e0a81477472
Marco Nenciarini <marco.nenciarini@enterprisedb.com>
2026-04-27 01:25:26 +0200

    feat(ip): assign copyright to the Linux Foundation (#63)
    Adopt the new attribution information for contributions to
    CloudNativePG:
    
    ```
    Copyright © contributors to CloudNativePG, established as
    CloudNativePG a Series of LF Projects, LLC.
    ```
    
    Adopt the SPDX format for Apache License 2.0
    
    Signed-off-by: Marco Nenciarini <marco.nenciarini@enterprisedb.com>

--- monitoring/setup.sh
+++ monitoring/setup.sh
@@ -1,26 +1,30 @@
 #!/usr/bin/env bash
+##
+## Copyright © contributors to CloudNativePG, established as
+## CloudNativePG a Series of LF Projects, LLC.
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+##     http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+##
+## SPDX-License-Identifier: Apache-2.0
+##
+
+#
+# Setup a Prometheus/Grafana stack on infrastructure nodes
 #
 # This script installs and configures the Prometheus and Grafana operators.
 # When run without arguments, it automatically detects all cnpg-playground
 # Kind clusters in your environment and deploys the monitoring stack for each.
 # To install monitoring for specific regions only, pass the region names as arguments.
-#
-#
-# Copyright The CloudNativePG Contributors
-#
-# Setup a Prometheus/Grafana stack on infrastructure nodes
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 #
 
 # Source the common setup script
